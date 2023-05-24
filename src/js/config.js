08fa8405d3085577e99e2be7d7f2f6f3738e2d24
Qhoan_Tran3008 <thaihoang03082003@gmail.com>
2023-05-24 21:31:51 +0700

    QHoan lastest version

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<YOUR_KEY>';
+export const KEY = '69fbb7f3-439c-46bf-9ee3-657a9f6e13c1';
 export const MODAL_CLOSE_SEC = 2.5;
